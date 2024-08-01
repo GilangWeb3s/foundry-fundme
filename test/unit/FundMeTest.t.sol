828e2d6024239b5f17f76af1b648e1db3f3ca526
Gilang <maulana.gilang305@gmail.com>
2024-08-01 12:55:29 +0700

    feat: initial commit

---
+++ test/unit/FundMeTest.t.sol
@@ -0,0 +1,28 @@
+//SPDX-Lisence-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() external {
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+    }
+
+    function testMinimumDollarIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsSender() public view {
+        assertEq(fundMe.i_owner(), msg.sender);
+    }
+
+    function testPriceVersion() public view {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
+    }
+}
